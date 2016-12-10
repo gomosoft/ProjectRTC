6cc9ec0e0111c42762a7b1455a5e14016f7ca20b
javier gomez <jgomez@twoconnect.com>
2016-12-10 07:18:53 +0100

    new events io

--- app/socketHandler.js
+++ app/socketHandler.js
@@ -19,24 +19,25 @@ module.exports = function(io, streams) {
       console.log('-- ' + client.id + ' is ready to stream --');
       
       streams.addStream(client.id, options.name); 
-      client.emit('stream::added', {id:client.id, name: options.name});
+      client.broadcast.emit('stream::added', {id:client.id, name: options.name});
 
     });
     
     client.on('update', function(options) {
       streams.update(client.id, options.name);
-      client.emit('stream::upated', {id:client.id, name: options.name});
+      client.broadcast.emit('stream::upated', {id:client.id, name: options.name});
 
     });
 
     function leave() {
       console.log('-- ' + client.id + ' left --');
       streams.removeStream(client.id);
-      client.emit('stream::left', {id:client.id});
+      client.broadcast.emit('stream::left', {id:client.id});
 
     }
 
     client.on('disconnect', leave);
     client.on('leave', leave);
+
   });
 };
