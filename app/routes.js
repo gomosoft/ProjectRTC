f8e7f8c650759c2d91327f2c4af21a08e5349498
javier gomez <jgomez@twoconnect.com>
2017-02-03 11:45:20 +0800

    last

--- app/routes.js
+++ app/routes.js
@@ -22,7 +22,7 @@ module.exports = function(app, streams) {
   };
 
   app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', req.get('Origin') || '*');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
   res.header('Access-Control-Expose-Headers', 'Content-Length');
