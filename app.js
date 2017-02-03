9cf5b8013c50dd637ce1f4db27de99928576c2dd
javier gomez <jgomez@twoconnect.com>
2017-02-03 12:31:31 +0800

    latest

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
   res.header('Access-Control-Expose-Headers', 'Content-Length');
-  res.header('Access-Control-Allow-Headers', 'Accept, Authorization, Content-Type, X-Requested-With, Range, Pragma');
+  res.header('Access-Control-Allow-Headers', 'Accept, Authorization, Content-Type, X-Requested-With, Range, Pragma, Cache-Control');
   if (req.method === 'OPTIONS') {
     return res.send(200);
   } else {
