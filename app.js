3bf1fff90e2322db575fc2c0636a33de466d7a3c
javier gomez <jgomez@twoconnect.com>
2017-02-03 11:07:03 +0800

    last

--- app.js
+++ app.js
@@ -29,6 +29,15 @@ app.use('/vendors',express.static(path.join(__dirname, 'bower_components')));
 if ('development' == app.get('env')) {
   app.use(errorHandler());
 }
+ 
+ app.use(function(req, res, next) {
+  
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+
+  next();
+  });
 
 // routing
 require('./app/routes.js')(app, streams);
--- app/routes.js
+++ app/routes.js
@@ -21,14 +21,7 @@ module.exports = function(app, streams) {
     res.status(200).json(data);
   };
 
- app.use(function(req, res, next) {
-  
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
 
-  next();
-  });
 
   app.get('/streams.json', displayStreams);
   app.get('/', index);
