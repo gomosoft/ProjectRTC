e8c28773c1b9a135a87ea2e2d3f9192c4085e5f9
javier gomez <jgomez@twoconnect.com>
2017-02-03 12:08:06 +0800

    latest

--- app.js
+++ app.js
@@ -30,21 +30,9 @@ if ('development' == app.get('env')) {
   app.use(errorHandler());
 }
 
-app.use(function(req, res, next) {
-  
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
-  res.header('Access-Control-Expose-Headers', 'Content-Length');
-  res.header('Access-Control-Allow-Headers', 'Accept, Authorization, Content-Type, X-Requested-With, Range');
-  
-  if (req.method === 'OPTIONS') {
-    return res.send(200);
-  } else {
-    return next();
-  }
+var cors = require('cors');
+app.use(cors);
 
-});
 
 // routing
 require('./app/routes.js')(app, streams);
