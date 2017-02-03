745d3397cf20400b212094f09bd6ece1eb691b36
javier gomez <jgomez@twoconnect.com>
2017-02-03 10:59:48 +0800

    last

--- app/routes.js
+++ app/routes.js
@@ -23,7 +23,7 @@ module.exports = function(app, streams) {
 
  app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Options, Content-Type, Accept");
   next();
   });
 
