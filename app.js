dcc2ea44b36bc65e97533ac93c186ab3467707f1
javier gomez <jgomez@twoconnect.com>
2017-05-18 00:00:30 +0700

    latest

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ function allowCors(req, res, next) {
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
   res.header('Access-Control-Expose-Headers', 'Content-Length');
-  res.header('Access-Control-Allow-Headers', 'Accept, Authorization, Content-Type, X-Requested-With, Range, Pragma, Cache-Control, If-Modified-Since');
+  res.header('Access-Control-Allow-Headers', 'Accept, Authorization, Content-Type, X-Requested-With, Range, Pragma, Cache-Control, If-Modified-Since, withCredentials');
   
   if (req.method === 'OPTIONS') {
     return res.send(200);
