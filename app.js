5f7240e8fa7b697ab5615cb9739921183658655a
javier gomez <jgomez@twoconnect.com>
2017-05-17 23:27:36 +0700

    latest

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ var server = app.listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
 
-var io = require('socket.io').listen(server);
+var io = require('socket.io').listen(server, { origins: '*:*'});
 /**
  * Socket.io event handling
  */
