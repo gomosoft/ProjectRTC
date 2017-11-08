364b6bc02cf7d9ac472292d834f988b710de56ab
Javier Gomez <jgomez@gomosoft.com>
2017-11-08 13:14:46 -0500

    all origins

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ var server = app.listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
 
-var io = require('socket.io').listen(server);
+var io = require('socket.io').listen(server, '0.0.0.0');
 /**
  * Socket.io event handling
  */
